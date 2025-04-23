ac8f386beccf15c12789f2e33c581edb61d851bb
LonelyOrc <lonelyorcproductions@gmail.com>
2025-04-23 06:14:09 -0400

    adding the updated primary themes for buttons and closing out build-screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -11,7 +11,7 @@ export default function Index() {
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} />
         <View style={styles.footerContainer}>
-          <Button label="Choose a photo" />
+          <Button theme="primary" label="Choose a photo" />
           <Button label="Use this photo" />
         </View>
       </View>
