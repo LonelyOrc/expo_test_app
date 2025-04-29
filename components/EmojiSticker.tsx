f616cd1d4d6748a1167303a89eb7e8b3bafc2021
LonelyOrc <lonelyorcproductions@gmail.com>
2025-04-29 06:14:28 -0400

    adding in spring animation to use with the gesture tap

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -10,6 +10,22 @@ type Props = {
 
 export default function EmojiSticker({ imageSize, stickerSource }: Props) {
     const scaleImage = useSharedValue(imageSize);
+    const doubleTap = Gesture.Tap()
+        .numberOfTaps(2)
+        .onStart(() => {
+            if (scaleImage.value !== imageSize * 2) {
+                scaleImage.value = scaleImage.value * 2;
+            } else {
+                scaleImage.value = Math.round(scaleImage.value / 2);
+            }
+        }
+        )
+    const imageStyle = useAnimatedStyle(() => {
+        return {
+            width: withSpring(scaleImage.value),
+            height: withSpring(scaleImage.value)
+        };
+    });
 
     return (
         <View style={{ top: -350 }}>
